cc58863c75723cca5dbda1a4ef33a21610b9794a
mgapp <mgapp@MIKEGYO>
2025-06-03 18:17:46 -0400

    Add project files.

---
+++ ForEach/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "Tim,Sue,Bob,Jane";
+            List<string> firstNames = data.Split(',').ToList();
+            foreach (string firstName in firstNames) 
+            {
+                Console.WriteLine(firstName);
+            }
+        }
+    }
+}
